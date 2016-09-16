d48105f6663abc16b96aee85a0b3ccb250d1ff13
Nicholas Bishop <nicholasbishop@gmail.com>
2016-09-16 04:44:46 -0400

    Lint fixes

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 
+# pylint: disable=missing-docstring
+
 from datetime import date
 from unittest import TestCase, main
 
@@ -9,7 +11,6 @@ class TestStash(TestCase):
     def test_date_prefix(self):
         self.assertEqual(make_date_prefix(date(year=1988, month=2, day=19)),
                          '1988/02-February/19/')
-
 
 if __name__ == '__main__':
     main()
