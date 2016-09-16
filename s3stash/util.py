054fa18637c498b1195b39a920a315691af5ecd4
Nicholas Bishop <nicholasbishop@gmail.com>
2016-09-16 04:45:03 -0400

    Add make_s3_client to util

--- s3stash/util.py
+++ s3stash/util.py
@@ -1,5 +1,8 @@
 """Functions used by the default implementation of the Stash class."""
 
+from boto3 import client
+
+
 def make_date_prefix(date):
     """Make an S3 key prefix from a date.
 
@@ -13,3 +16,14 @@ def make_date_prefix(date):
 
     """
     return date.strftime('%Y/%m-%B/%d/')
+
+
+def make_s3_client(credentials):
+    """Make a client for uploading to S3.
+
+    credentials: an orgtup.AwsCredentials object
+    """
+    return client(
+        's3',
+        aws_access_key_id=credentials.access_key_id,
+        aws_secret_access_key=credentials.secret_access_key)
