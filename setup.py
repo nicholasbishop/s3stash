cfd8d4835a1378aaa2ea12c2f915fce8093a729f
Nicholas Bishop <nicholasbishop@gmail.com>
2016-09-18 01:26:10 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read_readme():
 
 setup(
     name='s3stash',
-    version='1.1.1',
+    version='1.1.2',
     description='Very simple module that uses boto3 to stash a file in S3',
     long_description=read_readme(),
     url='https://github.com/nicholasbishop/s3stash',
