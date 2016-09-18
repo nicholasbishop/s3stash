eacb94167e70668c9272284c88eeeacf5115201d
Nicholas Bishop <nicholasbishop@gmail.com>
2016-09-18 01:25:50 -0400

    Add description and long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
+from os import path
 from setuptools import setup, find_packages
+
+
+SCRIPT_PATH = path.abspath(path.dirname(__file__))
+
+
+def read_readme():
+    """Get the contents of the README.rst file."""
+    readme_path = path.join(SCRIPT_PATH, 'README.rst')
+    with open(readme_path) as rfile:
+        return rfile.read()
+
 
 setup(
     name='s3stash',
     version='1.1.1',
+    description='Very simple module that uses boto3 to stash a file in S3',
+    long_description=read_readme(),
     url='https://github.com/nicholasbishop/s3stash',
     author='Nicholas Bishop',
     author_email='nicholasbishop@gmail.com',
