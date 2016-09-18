5f7fb13cc39d8a33a4b0aae64e94622b8680c9ad
Nicholas Bishop <nicholasbishop@gmail.com>
2016-09-18 01:19:26 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='s3stash',
-    version='1.1.0',
+    version='1.1.1',
     url='https://github.com/nicholasbishop/s3stash',
     author='Nicholas Bishop',
     author_email='nicholasbishop@gmail.com',
